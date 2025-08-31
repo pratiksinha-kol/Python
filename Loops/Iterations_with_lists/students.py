f58112b958a449a043dfbf16e132adb1263ea7be
Pratik Sinha <pratik.proriters@gmail.com>
2025-08-31 23:54:12 +0530

    Iteration with lists

---
+++ Loops/Iterations_with_lists/students.py
@@ -0,0 +1,22 @@
+
+
+students = ["Harry Potter", "Hermione Granger", "Ron Weasley", "Draco Malfoy"]
+
+print(f"Number of new students as per given list: {len(students)}\n")
+
+####################################
+# Iterate through the list of students and print a greeting for each student without using for loop
+# print(students[0])
+####################################
+
+for _ in students:
+    print("Hello, " + _ + "!")
+    # print("You are a student at Hogwarts School of Witchcraft and Wizardry.\n")
+
+
+
+# Redundant code below, not needed for the exercise
+#
+# length = len(students)
+# print(length)   
+###############################
