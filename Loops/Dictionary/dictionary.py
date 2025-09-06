9cac5544d50bb94f780390f1cce7af78aeba9709
Pratik Sinha <pratik.proriters@gmail.com>
2025-09-06 23:14:54 +0530

    Dictionary

---
+++ Loops/Dictionary/dictionary.py
@@ -0,0 +1,41 @@
+##########################################
+# Dictionary in Python is an unordered collection of data values, used to store data values like a map, which, unlike other Data Types that hold only a single value as an element, can hold multiple values.
+# A Dictionary is a collection which is ordered*, changeable and do not allow duplicates.
+# Dictionaries are written with curly brackets, and have keys and values.
+##########################################
+
+students = ["Harry Potter", "Hermione Granger", "Ron Weasley", "Draco Malfoy"]
+houses = ["Gryffindor", "Gryffindor", "Gryffindor", "Slytherin"]
+
+# Creating a dictionary using two lists
+# Using dictionary comprehension
+# 
+student_dict = {students[i]: houses[i] for i in range(len(students))}
+print(student_dict)
+
+
+# Manually creating a dictionary
+player_dict_manual = {
+    "Wayne Rooney": "Manchester United",
+    "Lionel Messi": "Barcelona",
+    "Paolo Maldini": "AC Milan",
+    "Diego Maradona": "Napoli"
+}
+print(f"The manual dictionary is as follows: {player_dict_manual}")
+print(player_dict_manual["Paolo Maldini"]) # Accessing value using key
+
+# Using loop to get values from dictionary
+for player in player_dict_manual:
+    print(player)
+
+first_name = ["Pratik", "Atindra", "Rohit", "Sourav", "Virat"]
+last_name = ["Sinha", "Sarkar", "Sharma", "Ganguly", "Kohli", "Dhoni"]
+
+# Using zip function to combine two lists into a dictionary
+# zip() function returns an iterator of tuples based on the iterable objects.
+# We can convert the iterator of tuples into a dictionary using dict() function
+# Here, first_name list will be the keys and last_name list will be the values
+# If the lists are of unequal length, zip() will stop creating pairs when the shortest list is exhausted.
+# In this case, last_name list has 6 elements while first_name list has 5 elements
+student_dict_zip = dict(zip(first_name, last_name))
+print(f"The dictionary created using zip function WITH UNEQUAL LENGTH: {student_dict_zip}")
