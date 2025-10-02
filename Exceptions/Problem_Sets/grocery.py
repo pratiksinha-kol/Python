a262deb026ec63f84cac4ac7b83a3d985a9de0c5
Pratik Sinha <pratik.proriters@gmail.com>
2025-10-02 18:53:18 +0530

    Changed the grocery program completely

--- Exceptions/Problem_Sets/grocery.py
+++ Exceptions/Problem_Sets/grocery.py
@@ -1,18 +1,15 @@
-# Implement a program where user inputs grocery items
-# It count how many times the items has been entered and the item
-
-
 grocery_list = {}
 
-try:
-    while True:
-        item = input("Item: ").strip().upper()
-        if item:
-            # If item is already in the dictionary, increment its count
-            # Otherwise, add it to the dictionary with a count of 1
-            # Using get method to simplify the code
-            grocery_list[item] = grocery_list.get(item, 0) + 1
-except EOFError:
-    print()
-    for item in sorted(grocery_list):
-        print(f"{grocery_list[item]} {item}")
+while True:
+    try:
+        item = input()
+        grocery_list[item] = grocery_list.get(item, 0) + 1
+
+    except EOFError:
+        break
+
+sorted_dict = {k: grocery_list[k] for k in sorted(grocery_list)}
+
+for item, count in sorted_dict.items():
+    print(f"{count} {item.upper()}")
+
