de6b3b9d4464d3c4a7c5e83bf29f447aedbbeeca
Pratik Sinha <pratik.proriters@gmail.com>
2025-11-09 22:08:23 +0530

    Statistics Library

---
+++ Libraries/average.py
@@ -0,0 +1,7 @@
+import statistics
+
+print(statistics.mean([100,90,95]))
+print(statistics.median([10, 20, 30, 40, 50]))
+print(statistics.mode([11, 22, 33, 44, 55]))
+
+print("Mean, median, and mode calculated successfully.")
