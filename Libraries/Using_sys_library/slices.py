f8d4979d113319d9cc5aad8ec4865cb2a3f5ee2d
Pratik Sinha <pratik.proriters@gmail.com>
2025-11-22 20:46:21 +0530

    Using slicing method to skip first argument and last argument

--- Libraries/Using_sys_library/slices.py
+++ Libraries/Using_sys_library/slices.py
@@ -7,7 +7,13 @@ if len(sys.argv) < 2:
     sys.exit("Too few arguments")
 
 # In the following line, we slice sys.argv to start from index 1, but not index 0
-# After the colon, we leave it empty to include all remaining elements
+# After the colon (argv[1:]), we leave it empty to include all remaining elements
 # This way, we avoid printing the script name and only print the actual arguments
 for i in (sys.argv[1:]):
     print("My name is", i)
+
+# Using the '-1' index to exclude the last argument as well
+# In the following line, we slice sys.argv to start from index 1 and go up to but not including the last element
+# This way, we avoid printing both the script name and the last argument
+for i in (sys.argv[1:-1]):
+    print("My modified name is", i)    
