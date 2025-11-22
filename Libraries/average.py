53893e0a8078913091e2932bbf1ba5ca6acf53ac
Pratik Sinha <pratik.proriters@gmail.com>
2025-11-22 19:34:19 +0530

    Using statistics library

--- Libraries/average.py
+++ Libraries/average.py
@@ -1,7 +1,7 @@
 import statistics
 
-print(statistics.mean([100,90,95]))
+print(f"The mean value is {statistics.mean([100,90,95])}")
 print(statistics.median([10, 20, 30, 40, 50]))
-print(statistics.mode([11, 22, 33, 44, 55]))
+print(statistics.mode([22, 22, 33, 33, 55]))
 
 print("Mean, median, and mode calculated successfully.")
