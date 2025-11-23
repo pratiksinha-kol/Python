844d309e690e8723c92169d30f22f7f4a763a663
Pratik Sinha <pratik.proriters@gmail.com>
2025-11-23 20:52:42 +0530

    Modified the test_hello file

--- Unit_Tests/Unit_Test_Hello/test_hello.py
+++ Unit_Tests/Unit_Test_Hello/test_hello.py
@@ -2,9 +2,12 @@
 from hello_program import hello
 
 def test_hello():
-    if hello("Alice") != "Hello, Alice":
-        raise AssertionError("Test failed for input 'Alice'")
+    for name in ["Bob", "Charlie", "Pratik"]:
+        assert hello(name) == f"Hello, {name}"
+        # assert hello(name) == f"Hello, {name}", f"Test failed for input '{name}'"
+    
+    # if hello("Alice") != "Hello, Alice":
+    #     raise AssertionError("Test failed for input 'Alice'")
 
 def test_hello_default():
-    if hello() != "Hello, World":
-        raise AssertionError("Test failed for default input")
+    assert hello() == "Hello, World"
